1d0c4d8c75ab781798afc1570f3aed866fdcea1d
JavaJavascriptPython <jellytelly32@gmail.com>
2024-09-17 16:35:26 +0530

    Update SQLiteConnection.java

--- SQLiteConnection.java
+++ SQLiteConnection.java
@@ -143,3 +143,31 @@ public class SQLiteSelectTest {
         }
     }
 }
+
+
+
+
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
+
+@RestController
+class MyController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Spring Boot without Maven!";
+    }
+}
